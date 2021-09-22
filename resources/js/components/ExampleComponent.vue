ef16ae0c8cdcc56927d1f498a265ff05dae3e56b
Asa Abbad <asa0abbad@gmail.com>
2021-09-22 22:39:00 +0300

    install Tailwind

--- resources/js/components/ExampleComponent.vue
+++ resources/js/components/ExampleComponent.vue
@@ -1,11 +1,11 @@
 <template>
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">Example Component</div>
+    <div class="">
+        <div class="">
+            <div class="">
+                <div class="">
+                    <div class="">Example Component</div>
 
-                    <div class="card-body">
+                    <div class="text-red-500">
                         I'm an example component.
                     </div>
                 </div>
